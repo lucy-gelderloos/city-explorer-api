0df2611f953a576601086e4262c8e6810ca8cddf
Lucy Gelderloos <lucygel@gmail.com>
2022-07-20 17:52:42 -0700

    Weather query works with lat & lon

--- server.js
+++ server.js
@@ -27,19 +27,26 @@ app.get('/', (request, response) => {
 });
 
 app.get('/weather', (request, response) => {
+  console.log('request.query', request.query);
   let cityName = request.query.cityName.toLowerCase();
-  let lat = request.query.lat;
-  let lon = request.query.lon;
+  let lat = parseInt(request.query.lat);
+  let lon = parseInt(request.query.lon);
+  console.log('weather line 34[0].lat', weather[0].lat, 'lat', lat, 'lon', lon);
 
   let weatherCity = weather.find(el => {
+    let weatherLat = Math.round(el.lat);
+    let weatherLon = Math.round(el.lon);
+    console.log('weatherLon', weatherLon);
+
+    // return cityName === el.city_name.toLowerCase();
     return cityName === el.city_name.toLowerCase()
-    && lat === Math.round(el.lat)
-    && lon === Math.round(el.lon);
+    && lat === weatherLat
+    && lon === weatherLon;
   });
-  // console.log('weatherCity', weatherCity);
+  console.log('weatherCity', weatherCity);
   let forecastArr = makeForecastArray(weatherCity);
 
-  if(cityName !== null) {
+  if(cityName) {
     response.send(forecastArr);
     console.log('forecastArr',forecastArr);
   } else {
