ef2f97ac3e789d304e218c5ab4fcd9cc3ce13f4e
Lucy Gelderloos <lucygel@gmail.com>
2022-07-20 22:35:59 -0700

    Movies render

--- server.js
+++ server.js
@@ -7,47 +7,20 @@ const cors = require('cors');
 const app = express();
 // const PORT = 3000;
 const PORT = process.env.PORT;
-const weather = require('./data/weather.json');
-const { query } = require('express');
-const e = require('express');
 
 
 app.use(cors());
 
 class Forecast {
-//   static weather = require('./data/weather.json');
   constructor(date, condition, high, low) {
     this.date = date;
     this.condition = condition;
     this.high = high;
     this.low = low;
     this.weatherAPIKey = process.env.REACT_APP_WEATHERBIT_API_KEY;
+    this.movieAPIKey = process.env.REACT_APP_TMDB_API_KEY;
   }
 }
-
-
-app.get('/', (request, response) => {
-  response.send('hello from the home route!');
-});
-
-app.get('/weather', (request, response) => {
-  console.log('request.query', request.query);
-
-  // let weatherAPIQuery = request.query.weatherAPIQuery;
-
-  let url = `https://api.weatherbit.io/v2.0/forecast/daily?&key=048b4716799246529c392254d49ff3dc&lat=${request.query.lat}&lon=${request.query.lon}`;
-
-  axios.get(url)
-    .then(res => {
-      console.log('res.data.data', res.data.data);
-      let forecastArr = makeForecastArray(res.data.data);
-      response.send(forecastArr);
-    })
-    .catch((e) => {
-      console.log(e);
-      response.status(500).send(e);
-    });
-});
 
 function makeForecastArray(weatherCity) {
   const forecastArr = weatherCity.map(el => {
@@ -59,6 +32,65 @@ function makeForecastArray(weatherCity) {
   });
   return forecastArr;
 }
+
+class Movie {
+  constructor(title, overview, releaseDate, popularity) {
+    this.title = title;
+    this.overview = overview;
+    this.releaseDate = releaseDate;
+    this.popularity = popularity;
+  }
+}
+
+function makeMoviesArray(city) {
+  const moviesArr = city.map(el => {
+    let title = el.title;
+    let overview = el.overview;
+    let releaseDate = el.release_date;
+    let popularity = el.popularity;
+    return new Movie(title, overview, releaseDate, popularity);
+  });
+  return moviesArr;
+}
+
+
+app.get('/', (request, response) => {
+  response.send('hello from the home route!');
+});
+
+app.get('/weather', (request, response) => {
+
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?&key=048b4716799246529c392254d49ff3dc&lat=${request.query.lat}&lon=${request.query.lon}`;
+
+  axios.get(url)
+    .then(res => {
+      // console.log('res.data.data', res.data.data);
+      let forecastArr = makeForecastArray(res.data.data);
+      response.send(forecastArr);
+    })
+    .catch((e) => {
+      console.log(e);
+      response.status(500).send(e);
+    });
+});
+
+app.get('/movies', (request, response) => {
+
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=5fc45765a1e23e490961b517caaaf372&query=${request.query.cityName}`;
+  console.log('movie url',url);
+
+  axios.get(url)
+    .then(res => {
+      console.log('res.data', res.data);
+      let moviesArr = makeMoviesArray(res.data.results);
+      // let forecastArr = makeForecastArray(res.data.data);
+      response.send(moviesArr);
+    })
+    .catch((e) => {
+      console.log(e);
+      response.status(500).send(e);
+    });
+});
 
 app.use('*', (error, request, response, next) => {
   response.send(500).send(error);
